f4e5b64d6a14a73d3e4a8884f1081849d6d243bb
unknown <you@example.com>
2024-09-27 22:20:23 +0100

    prepare base model

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from src.chicken_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.chicken_project.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from src.chicken_project import logger
 
 
-STAGE_NAME = "Data Ingestion stage"
-try:
-   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
-   data_ingestion = DataIngestionTrainingPipeline()
-   data_ingestion.main()
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
