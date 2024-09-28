6ce6904dbf7437ccc7874fc4edf1ef42855d2914
unknown <you@example.com>
2024-09-28 20:05:48 +0100

    cllbacks

--- src/chicken_project/components/prepare_base_model.py
+++ src/chicken_project/components/prepare_base_model.py
@@ -2,6 +2,7 @@ import os
 import urllib.request as request
 from zipfile import ZipFile
 import tensorflow as tf
+import time
 from pathlib import Path
 from src.chicken_project.entity.config_entity import PrepareBaseModelConfig
 
